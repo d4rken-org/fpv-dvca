dadf2c2ffed621fb47d0c254462a64c14d71f4da
darken <darken@darken.eu>
2021-05-23 00:24:50 +0200

    DVR wip

--- app/src/main/java/eu/darken/fpv/dvca/dvr/core/ffmpeg/FFmpegDvrRecorder.kt
+++ app/src/main/java/eu/darken/fpv/dvca/dvr/core/ffmpeg/FFmpegDvrRecorder.kt
@@ -1,48 +1,48 @@
-//package eu.darken.fpv.dvca.dvr.core.ffmpeg
-//
-//import android.content.Context
-//import android.net.Uri
-//import com.arthenica.ffmpegkit.FFmpegKit
-//import com.arthenica.ffmpegkit.FFmpegKitConfig
-//import dagger.hilt.android.qualifiers.ApplicationContext
-//import eu.darken.fpv.dvca.dvr.core.DvrRecorder
-//import okio.Source
-//import okio.buffer
-//import okio.sink
-//import java.io.FileOutputStream
-//import javax.inject.Inject
-//import kotlin.concurrent.thread
-//
-//class FFmpegDvrRecorder @Inject constructor(
-//    @ApplicationContext private val context: Context
-//) : DvrRecorder {
-//
-//    // TODO
-//    override fun record(source: Source, safUri: Uri): DvrRecorder.Session {
-//
-//        val pipe1 = FFmpegKitConfig.registerNewFFmpegPipe(context)
-////        val pipe2 = FFmpegKitConfig.registerNewFFmpegPipe(context)
-//
-//        val ffmpegTarget = FFmpegKitConfig.getSafParameterForWrite(context, safUri)
-//
-//        var recording = true
-//        thread {
-//            FFmpegKit.execute("-fflags nobuffer -f:v h264 -probesize 8192 -i $pipe1 -f mpegts -vcodec copy -preset ultrafast $ffmpegTarget")
-//        }
-//        thread {
-//            val ffmpegSink = FileOutputStream(pipe1).sink().buffer()
-//            source.use {
-//                val sour = it.buffer()
-//                while (recording) {
-//                    ffmpegSink.write(sour, 8192)
-//                }
-//            }
-//        }
-//        return object : DvrRecorder.Session {
-//            override fun stop() {
-//                recording = false
-//            }
-//
-//        }
-//    }
-//}
+package eu.darken.fpv.dvca.dvr.core.ffmpeg
+
+import android.content.Context
+import android.net.Uri
+import com.arthenica.ffmpegkit.FFmpegKit
+import com.arthenica.ffmpegkit.FFmpegKitConfig
+import dagger.hilt.android.qualifiers.ApplicationContext
+import eu.darken.fpv.dvca.dvr.core.DvrRecorder
+import okio.Source
+import okio.buffer
+import okio.sink
+import java.io.FileOutputStream
+import javax.inject.Inject
+import kotlin.concurrent.thread
+
+class FFmpegDvrRecorder @Inject constructor(
+    @ApplicationContext private val context: Context
+) : DvrRecorder {
+
+    // TODO
+    override fun record(source: Source, safUri: Uri): DvrRecorder.Session {
+
+        val pipe1 = FFmpegKitConfig.registerNewFFmpegPipe(context)
+//        val pipe2 = FFmpegKitConfig.registerNewFFmpegPipe(context)
+
+        val ffmpegTarget = FFmpegKitConfig.getSafParameterForWrite(context, safUri)
+
+        var recording = true
+        thread {
+            FFmpegKit.execute("-fflags nobuffer -f:v h264 -probesize 8192 -i $pipe1 -f mpegts -vcodec copy -preset ultrafast $ffmpegTarget")
+        }
+        thread {
+            val ffmpegSink = FileOutputStream(pipe1).sink().buffer()
+            source.use {
+                val sour = it.buffer()
+                while (recording) {
+                    ffmpegSink.write(sour, 8192)
+                }
+            }
+        }
+        return object : DvrRecorder.Session {
+            override fun stop() {
+                recording = false
+            }
+
+        }
+    }
+}
